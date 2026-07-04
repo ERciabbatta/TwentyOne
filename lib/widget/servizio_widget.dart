5faaaae7329178f512442cfa6f1bc96d0aec48f3
ERciabbatta <smassimosminni@gmail.com>
2026-07-04 13:16:45 +0200

    FIXES

--- lib/widget/servizio_widget.dart
+++ lib/widget/servizio_widget.dart
@@ -34,25 +34,3 @@ class ServizioWidget {
   }
 }
 
-/// Servizio per la gestione e l'aggiornamento del Widget della Home Screen.
-class ServizioWidget {
-  const ServizioWidget._();
-
-  /// Salva la streak corrente e i giorni rimanenti e richiede l'aggiornamento
-  /// del widget nativo (Android/iOS).
-  static Future<void> aggiornaWidget({
-    required int streak,
-    required int giorniRimanenti,
-  }) async {
-    try {
-      await HomeWidget.saveWidgetData<String>('streak', streak.toString());
-      await HomeWidget.saveWidgetData<String>('rimanenti', giorniRimanenti.toString());
-      await HomeWidget.updateWidget(
-        name: 'TwentyOneWidgetProvider',
-        androidName: 'TwentyOneWidgetProvider',
-      );
-    } catch (e) {
-      // Silenzioso in produzione
-    }
-  }
-}
